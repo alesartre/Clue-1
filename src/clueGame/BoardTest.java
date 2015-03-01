3d3394f868e9ec2e3ad04731d23de1a028e08f31
Tim Ross <tross@mines.edu>
2015-02-28 17:38:59 -0700

    BoardTest fixed and colored excel board added.

--- src/clueGame/BoardTest.java
+++ src/clueGame/BoardTest.java
@@ -1,8 +1,10 @@
 package clueGame;
 
 import static org.junit.Assert.*;
-import junit.framework.Assert;
 
+import java.util.Map;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,7 +14,10 @@ public class BoardTest {
 	
 	@Before
 	public void setUp(){
-		//testBoard = new Board();
+		ClueGame game = new ClueGame("ourBoard.csv", "Legend.txt");
+		game.loadConfigFiles();
+		testBoard = game.getBoard();
+		
 	}
 
 	@Test
@@ -23,32 +28,38 @@ public class BoardTest {
 	
 	@Test
 	public void roomLegend(){
-		Assert.assertTrue(testBoard.rooms.get("C") == "Cat Room");
-		Assert.assertTrue(testBoard.rooms.get("R") == "Room");
-		Assert.assertTrue(testBoard.rooms.get("L") == "Loomery");
-		Assert.assertTrue(testBoard.rooms.get("S") == "Sauna");
-		Assert.assertTrue(testBoard.rooms.get("H") == "Halfway House");
-		Assert.assertTrue(testBoard.rooms.get("O") == "Bob");
-		Assert.assertTrue(testBoard.rooms.get("D") == "Dog Room");
-		Assert.assertTrue(testBoard.rooms.get("B") == "Ballad Room");
-		Assert.assertTrue(testBoard.rooms.get("K") == "Kaligraphy Room");
-		Assert.assertTrue(testBoard.rooms.get("W") == "Walkway");
+		Map<Character, String> rooms = testBoard.getRooms();
+		Assert.assertEquals(rooms.get('C') , "Cat Room");
+		Assert.assertEquals(rooms.get('R') , "Room");
+		Assert.assertEquals(rooms.get('L'), "Loomery");
+		Assert.assertEquals(rooms.get('S'), "Sauna");
+		Assert.assertEquals(rooms.get('H'), "Halfway House");
+		Assert.assertEquals(rooms.get('O') , "Bob");
+		Assert.assertEquals(rooms.get('D'), "Dog Room");
+		Assert.assertEquals(rooms.get('B'), "Ballad Room");
+		Assert.assertEquals(rooms.get('K') , "Kaligraphy Room");
+		Assert.assertEquals(rooms.get('W') , "Walkway");
 		
 	}
 	
 	@Test
 	public void doorDirection(){
-		
+		BoardCell cell = testBoard.getCellAt(5, 12);
+		Assert.assertTrue(((RoomCell) cell).getDoorDirection() == RoomCell.DoorDirection.DOWN);
+		cell = testBoard.getCellAt(23, 0);
+		Assert.assertTrue( ((RoomCell) cell).getDoorDirection() == RoomCell.DoorDirection.UP);
+		cell = testBoard.getCellAt(3,20);
+		Assert.assertTrue( ((RoomCell)cell).getDoorDirection() == RoomCell.DoorDirection.LEFT);
 	}
 	
 	@Test 
 	public void roomInitials(){
 		
-	}
-	
-	@Test
-	public void exceptionHandling(){
-		
+		Assert.assertEquals('D', testBoard.getRoomCellAt(3,3).getInitial());
+		Assert.assertEquals('O', testBoard.getRoomCellAt(24,12).getInitial());
+		Assert.assertEquals('B', testBoard.getRoomCellAt(15,1).getInitial());
+		Assert.assertEquals('R', testBoard.getRoomCellAt(0,8).getInitial());
+
 	}
 	
 	
