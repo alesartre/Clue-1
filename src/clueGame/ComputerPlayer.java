95ee6050fedc1b1d2911a11e71a84b7d89115682
Andre Lesartre <alesartr@mymail.mines.edu>
2015-03-05 11:23:08 -0700

    Added Player and ComputerPlayer skeletons

--- src/clueGame/ComputerPlayer.java
+++ src/clueGame/ComputerPlayer.java
@@ -1,5 +1,20 @@
 package clueGame;
 
+import java.util.Set;
+
 public class ComputerPlayer extends Player{
+	private char lastRoomVisited;
+	
+	public void pickLocation(Set<BoardCell> targets) {
+		
+	}
+	
+	public void createSuggestion() {
+		
+	}
+	
+	public void updateSeen(Card seen) {
+		
+	}
 
 }
--- src/clueGame/Player.java
+++ src/clueGame/Player.java
@@ -1,5 +1,13 @@
 package clueGame;
 
+import java.util.ArrayList;
+
 public abstract class Player {
+	private String name;
+	private ArrayList<Card> cardList;
+	
+	public Card disproveSuggestion(String person, String room, String weapon) {
+		return null;
+	}
 
 }
