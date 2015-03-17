69b8eaefc9e98bfe57960d558ab4c8f5c4717fc2
Tim Ross <tross@mines.edu>
2015-03-16 22:20:15 -0600

    Card test changed, but still works

--- src/clueTests/GameSetupTests.java
+++ src/clueTests/GameSetupTests.java
@@ -18,12 +18,15 @@ public class GameSetupTests {
 
 	
 	private static ClueGame game;
-	
+	private static ArrayList<Card> testdeck;
 	@BeforeClass
 	public static void setup(){
 		game = new ClueGame("ClueLayout.csv", "ClueLegend.txt");
 		game.loadConfigFiles(); // This function creates the board, loads the rooms, cards, and the people
-		
+		testdeck = new ArrayList<Card>();
+		testdeck.add(new Card('P',"Barney Stinson"));
+		testdeck.add(new Card('W',"Ducky Tie"));
+		testdeck.add(new Card('R',"Metro News 1"));
 	}
 	
 	@Test
@@ -40,39 +43,9 @@ public class GameSetupTests {
 	public void loadCards(){
 		ArrayList<Card> deck = game.getDeck();
 		Assert.assertEquals(deck.size(), 21);
-		int p = 0;
-		int w = 0;
-		int r = 0;
-		boolean bs = false;
-		boolean dt = false;
-		boolean mn1 = false;
-		for( Card c: deck){
-			if (c.getCardType() == Card.CardType.PERSON){
-				p++;
-				System.out.println(c.getName());
-				if(c.getName().equals("Barney Stinson")){
-					bs = true;
-				}
-			}
-			else if(c.getCardType() == Card.CardType.WEAPON){
-				w++;
-				if(c.getName().equals("Ducky Tie")){
-					dt = true;
-				}
-			}
-			else if(c.getCardType() == Card.CardType.ROOM){
-				r++;
-				if(c.getName().equals("Metro News 1")){
-					mn1 = true;
-				}
-			}
-		}
-		Assert.assertEquals(p, 6);
-		Assert.assertEquals(w, 6);
-		Assert.assertEquals(r, 9);
-		Assert.assertEquals(bs, true);
-		Assert.assertEquals(dt, true);
-		Assert.assertEquals(mn1, true);
+		Assert.assertTrue(deck.contains(testdeck.get(0)));
+		Assert.assertTrue(deck.contains(testdeck.get(1)));
+		Assert.assertTrue(deck.contains(testdeck.get(2)));
 	}
 	
 }
