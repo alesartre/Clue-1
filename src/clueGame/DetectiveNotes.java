aa8558207b0224c904d4442b41c0571aa22b67dd
Patrick Sobolewski <psobolew@mymail.mines.edu>
2015-03-30 21:44:17 -0600

    Refactored DetectiveNotes to have fewer methods by making methods more generic.

--- src/clueGame/DetectiveNotes.java
+++ src/clueGame/DetectiveNotes.java
@@ -15,49 +15,30 @@ import clueGame.Card.CardType;
 
 public class DetectiveNotes extends JDialog {
 	private ClueGame game;
-	private Set<Card> weps;
-	private Set<Card> peeps;
-	private Set<Card> roomies;
 	private JButton hideButton;
-	
-	
+	private ArrayList<Card> deck;
+		
 	public DetectiveNotes(ClueGame game){
 		setTitle("Detective Notes");
 		setSize(600,660);
 		//setLayout(new GridLayout(3,2));
 		this.game = game;
-		
-		//Separate the deck into three smaller sets of weapon, people, and room cards
-		//to use to make different parts of the detective notes
-		ArrayList<Card> deck = game.getDeck();
-		weps = new HashSet<Card>();
-		peeps = new HashSet<Card>();
-		roomies = new HashSet<Card>();
-		for(Card c : deck){
-			if(c.getCardType()== CardType.WEAPON){
-				weps.add(c);
-			}
-			else if(c.getCardType()==CardType.PERSON){
-				peeps.add(c);
-			}
-			else if(c.getCardType()==CardType.ROOM){
-				roomies.add(c);
-			}
-		}
+		deck = game.getDeck();
+
 		JPanel mainPanel = new JPanel();
 		GridLayout grid = new GridLayout(0,2);
 		mainPanel.setLayout(grid);
-		JPanel people = createPeople();
-		JPanel weapons = createWeapons();
-		JPanel rooms = createRooms();
-		JPanel personGuess = createPersonGuess();
-		JPanel weaponGuess = createWeaponGuess();
-		JPanel roomGuess = createRoomGuess();
+		
+		JPanel people = createCheckBox(Card.CardType.PERSON);
+		JPanel rooms = createCheckBox(Card.CardType.ROOM);
+		JPanel weapons = createCheckBox(Card.CardType.WEAPON);
+		JPanel personGuess = createComboBox(Card.CardType.PERSON);
+		JPanel weaponGuess = createComboBox(Card.CardType.ROOM);
+		JPanel roomGuess = createComboBox(Card.CardType.WEAPON);
+		
 		hideButton = new JButton("HIDE");
 		hideButton.addActionListener(new ButtonListener());
-		
-		
-		
+			
 		mainPanel.add(people);
 		mainPanel.add(personGuess, BorderLayout.NORTH);
 		mainPanel.add(weapons);
@@ -68,69 +49,30 @@ public class DetectiveNotes extends JDialog {
 		add(hideButton, BorderLayout.SOUTH);
 		
 		setVisible(true);
-		
 	}
 	
-	private JPanel createPeople(){
+	public JPanel createCheckBox(Card.CardType t) {
 		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder (new EtchedBorder(), "People"));
-		for( Card p: peeps){
-			panel.add(new JCheckBox(p.getName()));
-		}
-		return panel;
-		
-	}
-	
-	private JPanel createWeapons(){
-		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder (new EtchedBorder(), "Weapons"));
-		for(Card c: weps){
-			panel.add(new JCheckBox(c.getName()));
-		}
-		return panel;
-		
-	}
-	
-	private JPanel createRooms(){
-		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder (new EtchedBorder(), "Weapons"));
-		for(Card c: roomies){
-			panel.add(new JCheckBox(c.getName()));
+		panel.setBorder(new TitledBorder (new EtchedBorder(), panel.getName()));
+		for(Card c: deck) {
+			if(c.getCardType() == t) {
+				panel.add(new JCheckBox(c.getName()));
+			}
 		}
 		return panel;
 	}
 	
-	private JPanel createPersonGuess(){
+	public JPanel createComboBox(Card.CardType t) {
 		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder (new EtchedBorder(), "Person Guess"));
-		JComboBox<String> combo = new JComboBox<String>();
-		for(Card c: peeps){
-			combo.addItem(c.getName());
+		panel.setBorder(new TitledBorder (new EtchedBorder(), panel.getName()));
+		JComboBox box = new JComboBox();
+		box.addItem("Uncertain");
+		for(Card c: deck) {
+			if(c.getCardType() == t) {
+				box.addItem(c.getName());
+			}
 		}
-		panel.add(combo);
-		return panel;
-	}
-	
-	private JPanel createWeaponGuess(){
-		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder (new EtchedBorder(), "Weapons Guess"));
-		JComboBox<String> combo = new JComboBox<String>();
-		for(Card c: weps){
-			combo.addItem(c.getName());
-		}
-		panel.add(combo);
-		return panel;
-		
-	}
-	
-	private JPanel createRoomGuess(){
-		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder (new EtchedBorder(), "RoomGuess"));
-		JComboBox<String> combo = new JComboBox<String>();
-		for(Card c: peeps){
-			combo.addItem(c.getName());
-		}
-		panel.add(combo);
+		panel.add(box);
 		return panel;
 	}
 	
@@ -142,9 +84,7 @@ public class DetectiveNotes extends JDialog {
 		public void actionPerformed(ActionEvent e) {
 			if(e.getSource() == hideButton){
 				setVisible(false);
-			}
-			
+			}	
 		}
-		
 	}
 }
